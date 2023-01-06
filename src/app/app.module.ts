954a6593684cce580afafe1967508ebbef451f66
Michell2022 <sistemas.angel04@gmail.com>
2023-01-06 10:36:16 -0500

    modulos y componentes + Dir creados

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CondicionalSiDirective } from './directivas/condicional-si.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CondicionalSiDirective
   ],
   imports: [
     BrowserModule,
---
+++ src/app/directivas/condicional-si.directive.spec.ts
@@ -0,0 +1,8 @@
+import { CondicionalSiDirective } from './condicional-si.directive';
+
+describe('CondicionalSiDirective', () => {
+  it('should create an instance', () => {
+    const directive = new CondicionalSiDirective();
+    expect(directive).toBeTruthy();
+  });
+});
